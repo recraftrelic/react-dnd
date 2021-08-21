04235235b9d740f3615980ff1537691514607f38
Manoj Singh Negi <justanothermanoj@gmail.com>
2021-08-21 10:34:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import { closestCenter, DndContext, PointerSensor, useSensor } from '@dnd-kit/core';
+import { arrayMove, SortableContext, useSortable, verticalListSortingStrategy } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { useState } from 'react';
 import './App.css';
 
+function SortableComponent ({
+  id,
+  name
+}) {
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transition,
+    transform,
+  } = useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition
+  }
+
+  return <p style={style} ref={setNodeRef} {...attributes} {...listeners}>{name}</p>
+}
+
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: "1",
+      name: "Manoj"
+    },
+    {
+      id: "2",
+      name: "John"
+    }
+  ]);
+
+  const sensors = [useSensor(PointerSensor)];
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.findIndex(item => item.id === active.id);
+        const newIndex = items.findIndex(item => item.id === over.id);
+        
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
+        <SortableContext
+          items={items.map(item => item.id)}
+          strategy={verticalListSortingStrategy}
         >
-          Learn React
-        </a>
-      </header>
+          {
+            items.map(item => <SortableComponent {...item} key={item.id} />)
+          }
+        </SortableContext>
+      </DndContext>
     </div>
   );
 }
